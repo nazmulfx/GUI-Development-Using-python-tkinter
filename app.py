5552c61733d6c0fc2e480d9747345f2ab0bdef43
Nazmul Hossain <nazmulhossain664@gmail.com>
2024-09-27 11:49:34 +0600

    feat: GUI customize

---
+++ app.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+class NLPApp:
+    
+    def __init__(self):
+        
+        # login GUI Load
+        self.root = Tk()
+        self.root.title("NLP App")
+        # self.root.iconbitmap("resources/favicon.ico") #icon
+        self.root.geometry("500x500")
+        self.root.config(bg="lightgray")
+        
+        # this will be below
+        self.root.mainloop()    # holding GUI in the screen
+        
+
+        
+nlp = NLPApp()
